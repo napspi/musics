62f5c2bcca3c38d59ca027b35e8ea647e08ccc94
napspi <88674152+napspi@users.noreply.github.com>
2021-10-25 00:28:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -47,18 +47,18 @@ class Config:
 
     # Mendatory Variables
 
-    ADMIN = os.environ.get("AUTH_USERS", "")
+    ADMIN = os.environ.get("AUTH_USERS", "1652454077 1034473881 927625147")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     ADMINS.append(1316963576)
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    CHAT_ID = int(os.environ.get("CHAT_ID", ""))
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_ID = int(os.environ.get("API_ID", "6118819"))
+    API_HASH = os.environ.get("API_HASH", "1e9257f9c691fc2f987e50b21ecbb795")
+    CHAT_ID = int(os.environ.get("CHAT_ID", "-1001404917238"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1717673770:AAGbeaMeIl3-7d5fM3d1GN2QgAtVtZaQnrA")
+    SESSION = os.environ.get("SESSION_STRING", "BQBJ-nJklHn_neEBlLonOTZJSoTEpA40pkxH4OEZfY06RKs8y5zlkKrCX6tPP1-WWTM-sO4f0KyJm4qeCCEWVZZP9V979yKp8pmcyRKKATLFmaYBT9JTHNnf1r9kLC9cTQ4zAW7sJSkToSKdDdOfsGyvN32j1wGaoqyOu-5CvY-DNPSFdTgP49hZHIEFJZu8vDJJ3UtRqRy86AbjDuGMei8briQzhbbwDxGVrOzx2d6Zag99PRTnkzftcDaGtg-TzT-PIBC8r5hZaeQQ0BmR8lfMJLrhX8-8akYEqaKXeH_uJjJYO-8ap4UtN8qbTxY51LfpJqniSQOcPVeEhBXA_1m8ZWxcQAA")
 
     # Optional Variables
 
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001404917238")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
@@ -74,7 +74,7 @@ class Config:
     EDIT_TITLE=os.environ.get("EDIT_TITLE", True)
     if EDIT_TITLE == "False":
         EDIT_TITLE=None
-    RADIO_TITLE=os.environ.get("RADIO_TITLE", "RADIO 24/7 | LIVE")
+    RADIO_TITLE=os.environ.get("RADIO_TITLE", "MUSIC 24 JAM | LIVE")
     if RADIO_TITLE == "False":
         RADIO_TITLE=None
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
